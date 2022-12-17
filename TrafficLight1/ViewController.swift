2453737d12772a0cc36d8f1b1f9b62c68bbf1c22
Yuri <krasnov1yuri@gmail.com>
2022-12-17 23:44:52 +0300

    Add IBOutlets

--- TrafficLight1/ViewController.swift
+++ TrafficLight1/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButtonPressed() {
+    }
+    
 }
 
